8e6f211af3d5d55ea10dc2875f109c5bbeeb794d
mansipatel1012 <manmit1612@gmail.com>
2023-05-29 23:22:03 +0100

    Initial commit

---
+++ src/main/java/com/restful/booker/restfullbooker/utils/Global.java
@@ -0,0 +1,6 @@
+package com.restful.booker.restfullbooker.utils;
+
+public class Global {
+    public static final String DEFAULT_USER_NAME = "admin";
+    public static final String DEFAULT_PASSWORD = "passsword123";
+}
